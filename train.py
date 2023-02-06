227445b0414662509485c849baa77e841baf24c8
FilippoBotti <filippo.botti.itis@gmail.com>
2023-02-06 02:17:29 +0100

    Everything works fine

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ def main(args):
     BATCH_SIZE = 1 # increase / decrease according to GPU memeory
     NUM_EPOCHS = 10 # number of epochs to train for
     NUM_WORKERS = 0
-    DEVICE = torch.device('mps') if torch.cuda.is_available() else torch.device('cpu')
+    DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     IMAGE_SIZE=[256,256]
 
     # classes: 0 index is reserved for background
@@ -91,7 +91,7 @@ def main(args):
     # # params = [p for p in model.parameters() if p.requires_grad]
     # # optimizer = torch.optim.AdamW(params=model.parameters(), lr=1e-5)
 
-    device = torch.device("cpu")
+    device = torch.device("cuda")
     # print("Device: ", device)
 
     # define solver class
