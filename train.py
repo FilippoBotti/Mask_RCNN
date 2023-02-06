e09e9f09a50c347ea921a9c23a8851711d4be137
FilippoBotti <filippo.botti.itis@gmail.com>
2023-02-06 02:13:25 +0100

    Everything works fine

--- train.py
+++ train.py
@@ -38,7 +38,7 @@ def main(args):
     BATCH_SIZE = 1 # increase / decrease according to GPU memeory
     NUM_EPOCHS = 10 # number of epochs to train for
     NUM_WORKERS = 0
-    DEVICE = torch.device('mps') if torch.cuda.is_available() else torch.device('cpu')
+    DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     IMAGE_SIZE=[256,256]
 
     # classes: 0 index is reserved for background
