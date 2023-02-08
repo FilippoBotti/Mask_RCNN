f44cabef33cfad23607cdf77650463112a8b1dcc
FilippoBotti <filippo.botti.itis@gmail.com>
2023-02-08 15:45:48 +0100

    Fixed solver

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ def main(args):
     else:
         DEVICE = torch.device("cpu")
 
-    IMAGE_SIZE=[67,100]
+    IMAGE_SIZE=[400,600]
 
     # classes: 0 index is reserved for background
     CLASSES = [
